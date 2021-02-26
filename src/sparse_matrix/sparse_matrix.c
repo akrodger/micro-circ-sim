554ecfe798912db4412cd0c97308e0416659c0bd
Bram Rodgers <akrodger@ucsc.edu>
2021-02-25 22:33:38 -0800

    Generalized BICGSTAB algorithm to work for a general linear map defined through a function pointer.

--- src/sparse_matrix/sparse_matrix.c
+++ src/sparse_matrix/sparse_matrix.c
@@ -14,15 +14,19 @@
  * Locally used helper functions:
  */
 
+void mcs_apply_linmap(double* x, double* y);
+
 /*
  * Static Local Variables:
  */
+
+mcs_spmat* lin_map;
 
 /*
  * Function Implementations:
  */
 
-void mcs_spmatvec(char tran, mcs_spmat* A, double* x, double*y){
+void mcs_spmatvec(char tran, mcs_spmat* A, double* x, double* y){
     long* r_arr;
     long* c_arr;
     long i, nr;//, nc;
@@ -44,34 +48,39 @@ void mcs_spmatvec(char tran, mcs_spmat* A, double* x, double*y){
         y[r_arr[i]] += A->dat[i]*x[c_arr[i]];
     }
 }
-#include"stdio.h"
-void mcs_spmat_bicgstab(mcs_spmat* A,
+
+void mcs_apply_lin_map(double* x, double* y){
+    mcs_spmatvec('n',lin_map,x,y);
+}
+
+void mcs_bicgstab(void (*L)(double*,double*),
                         double* b,
                         double* x,
                         double* work,
+                        long N,
                         double tol){
     /********From Xianyi Zeng's lecture notes at UT El Paso*********/
     double a, w, be, rho_old, rho_new, norm2;
     double *r_j, *r_0, *p_j, *v_j, *s_j, *t_j;
-    long N = A->r_len, i=0,k=0;
+    //long N = A->r_len;
+    long i=0;
     r_j = work;
     p_j = &(work[N]);
     v_j = &(work[2*N]);
     s_j = &(work[3*N]);
     t_j = &(work[4*N]);
     r_0 = &(work[5*N]);
-    mcs_spmatvec('n',A,x,r_0);
+    L(x,r_0);
     mcs_vector_add(b,r_0,-1.0,r_0,i,N);
     mcs_vector_copy(r_0,r_j,i,N);
     mcs_vector_copy(r_0,p_j,i,N);
     mcs_vector_dot(r_j,r_0,rho_old,i,N);
     do{
-        printf("iter = %ld\n",++k);
-        mcs_spmatvec('n',A,p_j,v_j);
+        L(p_j,v_j);
         mcs_vector_dot(v_j,r_0,a,i,N);
         a = rho_old / a;
         mcs_vector_add(r_j,v_j,-a,s_j,i,N);
-        mcs_spmatvec('n',A,s_j,t_j);
+        L(s_j,t_j);
         mcs_vector_dot(t_j,t_j,norm2,i,N);
         mcs_vector_dot(t_j,s_j,w,i,N);
         w = w/norm2;
@@ -86,6 +95,16 @@ void mcs_spmat_bicgstab(mcs_spmat* A,
         mcs_vector_combo2(r_j,p_j,be,v_j,w,p_j,i,N);
     }while(1);
     /****************END LECTURE NOTES REFERENCE********************/
+}
+
+void mcs_spmat_bicgstab(mcs_spmat* A,
+                        double* b,
+                        double* x,
+                        double* work,
+                        double tol){
+    extern mcs_spmat* lin_map;
+    lin_map = A;
+    mcs_bicgstab(&mcs_apply_lin_map,b,x,work,A->r_len,tol);
 }
 
 
--- src/sparse_matrix/sparse_matrix.h
+++ src/sparse_matrix/sparse_matrix.h
@@ -44,7 +44,33 @@ typedef struct _mcs_spmat{
  * If tran = 't' or 'T' then y = A^(T) * x is computed. Otherwise y = A * x
  * is computed.
  */
-void mcs_spmatvec(char tran, mcs_spmat* A, double* x, double*y);
+void mcs_spmatvec(char tran, mcs_spmat* A, double* x, double* y);
+
+
+
+/*
+ * For a linear map T, solve the system of equations T(x) = b for a
+ * given b using the stabilized biconjugate gradient method.
+ *
+ * T(x) is defined with the function pointer L. Given an input v, the output
+ * is the second argument of L. So if T(v)[i] = w[i],
+ * this is computed by L(v,w).
+ *
+ * BICGSTAB halts when approximated residual is less than tol.
+ *
+ * Expected: (# entries of x) = (# entries of b) = N.
+ * workspace vector called work expected to have 6*(# entries of x)
+ * memory allocated.
+ *
+ * x is expected to contain an intial guess for the solution to the system.
+ * Therefore, it is required to set the entries of x before calling.
+ */
+void mcs_bicgstab(void (*L)(double*,double*),
+                        double* b,
+                        double* x,
+                        double* work,
+                        long N,
+                        double tol);
 
 /*
  * For the sparse matrix A, solve the system of equations A*x = b for a given b
